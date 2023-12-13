c572540c64373dd5bd14fa0e12090a324a4ef418
Deepansh Bhargava <deepansh940@gmail.com>
2023-12-13 22:17:34 +0530

    refactored layout and added nvmrc

--- src/app/page.js
+++ src/app/page.js
@@ -5,82 +5,14 @@ import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   UploadOutlined,
-  UserOutlined,
   VideoCameraOutlined,
+  UserOutlined,
+  BellOutlined,
 } from '@ant-design/icons'
 
-import { Layout, Menu, Button, theme } from 'antd'
-const { Header, Sider, Content } = Layout
+import { Badge, Layout, Menu, Button, theme } from 'antd'
 
 const App = () => {
-  const [collapsed, setCollapsed] = useState(false)
-
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken()
-
-  return (
-    <Layout
-      style={{
-        height: '100vh',
-      }}
-    >
-      <Sider trigger={null} collapsible collapsed={collapsed} className="pt-14">
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-        />
-      </Sider>
-      <Layout>
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        >
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          />
-        </Header>
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-          }}
-        >
-          Content
-        </Content>
-      </Layout>
-    </Layout>
-  )
+  return <div>Dashboard</div>
 }
 export default App
